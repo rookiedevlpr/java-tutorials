b9d71e26d9cddc68db8372911c201d40922f6c30
Your Name <you@example.com>
2022-02-16 19:54:21 +0000

    hey again

---
+++ app.js
@@ -0,0 +1,117 @@
+//console.log("hello");
+
+
+//bulleans
+
+
+//let varBoo = false;
+
+//console.log(typeof varBoo);
+
+
+//strings
+//let varString = "attila's";
+//let varName ="yilmaz";
+//let varSpace = " ";
+
+//["a","t","t","i","l","a"];
+
+//console.log(10=="10");
+//console.log(10!="10.1");
+//console.log(10!="10");
+//.log(10==="10");
+
+
+//const varTernary = true;
+//AARAYS
+
+
+
+//console.log= [1,"2",true,1234];
+
+
+
+
+//2
+//onst varArr1=[1,"2","ELEMENT","SWITCH"]
+
+//console.log(arr1.length);
+
+
+//arr1[10] = Gino
+//arr1.push(5);
+
+//arr1.pop(4)
+//arr1.shift();
+
+
+
+//arr1=[]console.log(arr
+
+
+//const varArr1 = any["bread","black tea","Eggs,];
+//arr1
+//arr1("cereal","juice","coffe") = "breakfast";
+
+//console.log(arr1.length);
+
+//const breakfastArr = [] ;
+
+//let breakfastItem1 = "salad",
+  //  breakfastItem2 = "cheese",
+   // breakfastItem3 = "cereal",
+   // breakfastItem4 = "coffee";
+
+   // breakfastArr.push(breakfastItem1);
+    //breakfastArr.push(breakfastItem2);
+    //breakfastArr.push(breakfastItem3);
+    ///breakfastArr.push(breakfastItem4);
+
+//console.log(breakfastArr);
+//console.log(breakfastArr.includes("coffee"));
+//console.log(breakfastArr.indexOf("salad"));
+
+
+//const arr = [];
+
+//arr.push(
+//    {
+//firstName:"foo",
+//lastName: "bar",
+//    },
+ //   {
+    //firstName:"foo",
+//lastName: "bar",
+  //  },
+
+
+//)
+//console.log
+//age = 21,
+
+
+
+//console.log(obj1.lastName);
+
+
+
+
+//const spacecraft = {
+
+//obj1.notsomuchfast= ['veryLarge','veryJupiter,'raven']
+
+//obj2.hellveryFast = ['veryveryFast','veryAndromeda','hellveryFast','city'];
+
+
+
+//obj.obinside =  (key1:'veryLarge',key2:'veryJupiter'key3:);
+
+
+
+
+console.log
+
+
+
+
+
